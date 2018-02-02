345dff5344f0e0319aeb6d723f74f1b6ade2fca2
carlosm <carlosjmr12@gmail.com>
2018-02-02 13:49:00 -0400

    #agregados floatbuttons

--- app/src/main/java/com/example/marci/hf1/perfil.java
+++ app/src/main/java/com/example/marci/hf1/perfil.java
@@ -6,9 +6,20 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class perfil extends AppCompatActivity {
 
+    //boolean flag to know if main FAB is in open or closed state.
+    private boolean fabExpanded = false;
+    private FloatingActionButton fabSettings;
+
+    //Linear layout holding the Save submenu
+    private LinearLayout layoutFabSave;
+
+    //Linear layout holding the Edit submenu
+    private LinearLayout layoutFabEdit;
+    private LinearLayout layoutFabPhoto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,14 +27,51 @@ public class perfil extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        fabSettings = (FloatingActionButton) this.findViewById(R.id.fab);
+
+        layoutFabSave = (LinearLayout) this.findViewById(R.id.layoutFabSave);
+        layoutFabEdit = (LinearLayout) this.findViewById(R.id.layoutFabSave1);
+        layoutFabPhoto = (LinearLayout) this.findViewById(R.id.layoutFabSave2);
+
+        //When main Fab (Settings) is clicked, it expands if not expanded already.
+        //Collapses if main FAB was open already.
+        //This gives FAB (Settings) open/close behavior
+        fabSettings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if (fabExpanded == true){
+                    closeSubMenusFab();
+                } else {
+                    openSubMenusFab();
+                }
             }
         });
+
+        //Only main FAB is visible in the beginning
+        closeSubMenusFab();
     }
+
+
+    //closes FAB submenus
+    private void closeSubMenusFab(){
+        layoutFabSave.setVisibility(View.INVISIBLE);
+        layoutFabEdit.setVisibility(View.INVISIBLE);
+        layoutFabPhoto.setVisibility(View.INVISIBLE);
+        fabSettings.setImageResource(R.drawable.ic_ring_volume_white_24px);
+        fabExpanded = false;
+    }
+
+    //Opens FAB submenus
+    private void openSubMenusFab(){
+        layoutFabSave.setVisibility(View.VISIBLE);
+        layoutFabEdit.setVisibility(View.VISIBLE);
+        layoutFabPhoto.setVisibility(View.VISIBLE);
+        //Change settings icon to 'X' icon
+        fabSettings.setImageResource(R.drawable.ic_clear_white_24px);
+        fabExpanded = true;
+    }
+
+
+
 
 }
