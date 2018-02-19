c48b21fcba7c1ad4e1c37a308dc97e6e476fd73f
Miguel Sanchez <sanchezmj228@gmail.com>
2018-02-19 17:30:37 -0400

    listo cercanos, falta categorias y lo que esta en toDO.

---
+++ app/src/main/java/com/example/marci/hf1/categoria_result.java
@@ -0,0 +1,333 @@
+package com.example.marci.hf1;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class categoria_result extends AppCompatActivity {
+
+    // json object response url
+    private String urlJsonObj = "https://api.androidhive.info/volley/person_object.json";
+
+    private String urlJsonArry = "http://www.ksfactory.com.ve/cercanos.php?cercat=&cat=";
+
+    private static String TAG = MainActivity.class.getSimpleName();
+
+    // Progress dialog
+    private ProgressDialog pDialog;
+
+    public  Double lon, lat;
+
+    // temporary string to show the parsed response
+    private String jsonResponse;
+
+    String idservidor,cat,nombre,ciudad,estado,descripcion,precio,formapago,experiencia,zonas,reputacion,telefono;
+    Double latitud,longitud,distance;
+    public String cate;
+    public static ArrayList<Integer> images;
+    public static ArrayList<String> nombres,profesiones,ciudades,distances,cats;
+    public GridView gridView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nuevocer);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        gridView = findViewById(R.id.grid);
+        cate = getIntent().getExtras().getString("categoria");
+        lon=MainActivity.lon;
+        lat=MainActivity.lat;
+        pDialog = new ProgressDialog(categoria_result.this);
+        pDialog.setMessage("Filtrando cercanos...");
+        pDialog.setCancelable(false);
+        makeJsonArrayRequest();
+        System.out.println(jsonResponse);
+
+
+      /*  FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+    }
+
+    //dialog
+    private void showpDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hidepDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
+    }
+
+
+
+    //object y array request:
+
+    private void makeJsonObjectRequest() {
+
+        showpDialog();
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                urlJsonObj, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d(TAG, response.toString());
+
+                try {
+                    // Parsing json object response
+                    // response will be a json object
+                    String name = response.getString("name");
+                    String email = response.getString("email");
+                    JSONObject phone = response.getJSONObject("phone");
+                    String home = phone.getString("home");
+                    String mobile = phone.getString("mobile");
+
+                    jsonResponse = "";
+                    jsonResponse += "Name: " + name + "\n\n";
+                    jsonResponse += "Email: " + email + "\n\n";
+                    jsonResponse += "Home: " + home + "\n\n";
+                    jsonResponse += "Mobile: " + mobile + "\n\n";
+
+                    Toast.makeText(getApplicationContext(), ""+jsonResponse, Toast.LENGTH_LONG).show();
+                    // txtResponse.setText(jsonResponse);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(),
+                            "Error: " + e.getMessage(),
+                            Toast.LENGTH_LONG).show();
+                }
+                hidepDialog();
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_SHORT).show();
+                // hide the progress dialog
+                hidepDialog();
+            }
+        });
+
+        // Adding request to request queue
+        jsonObjReq.setShouldCache(false);
+        Volley.newRequestQueue(getApplicationContext()).add(jsonObjReq);
+    }
+
+
+
+
+
+    private void makeJsonArrayRequest() {
+
+        showpDialog();
+        String urlfinal= urlJsonArry+cate+"&lat="+lat+"&lng="+lon;
+        System.out.println(urlfinal);
+        // Toast.makeText(getContext(), ""+urlfinal, Toast.LENGTH_LONG).show();
+        JsonArrayRequest req = new JsonArrayRequest(urlfinal,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+
+                        int imagenesdefault [] = {R.drawable.launo,R.drawable.lados,R.drawable.latres,R.drawable.lacuatro,R.drawable.lacinco,R.drawable.laseis,
+                                R.drawable.lasiete,R.drawable.laocho,R.drawable.launo,
+                                R.drawable.launo,R.drawable.lados,R.drawable.latres,R.drawable.lacuatro,R.drawable.lacinco,R.drawable.laseis,
+                                R.drawable.lasiete,R.drawable.laocho,R.drawable.launo,
+                                R.drawable.launo,R.drawable.lados,R.drawable.latres,R.drawable.lacuatro,R.drawable.lacinco,R.drawable.laseis,
+                                R.drawable.lasiete,R.drawable.laocho,R.drawable.launo,
+                                R.drawable.launo,R.drawable.lados,R.drawable.latres,R.drawable.lacuatro,R.drawable.lacinco,R.drawable.laseis,
+                                R.drawable.lasiete,R.drawable.laocho,R.drawable.launo,
+                                R.drawable.launo,R.drawable.lados,R.drawable.latres,R.drawable.lacuatro,R.drawable.lacinco,R.drawable.laseis,
+                                R.drawable.lasiete,R.drawable.laocho,R.drawable.launo,
+                                R.drawable.launo,R.drawable.lados,R.drawable.latres,R.drawable.lacuatro,R.drawable.lacinco,R.drawable.laseis,
+                                R.drawable.lasiete,R.drawable.laocho,R.drawable.launo,
+                                R.drawable.launo,R.drawable.lados,R.drawable.latres,R.drawable.lacuatro,R.drawable.lacinco,R.drawable.laseis,
+                                R.drawable.lasiete,R.drawable.laocho,R.drawable.launo,
+                                R.drawable.launo,R.drawable.lados,R.drawable.latres,R.drawable.lacuatro,R.drawable.lacinco,R.drawable.laseis,
+                                R.drawable.lasiete,R.drawable.laocho,R.drawable.launo,
+                                R.drawable.launo,R.drawable.lados,R.drawable.latres,R.drawable.lacuatro,R.drawable.lacinco,R.drawable.laseis,
+                                R.drawable.lasiete,R.drawable.laocho,R.drawable.launo,
+                                R.drawable.launo,R.drawable.lados,R.drawable.latres,R.drawable.lacuatro,R.drawable.lacinco,R.drawable.laseis,
+                                R.drawable.lasiete,R.drawable.laocho,R.drawable.launo};
+
+                        try {
+                            // Parsing json array response
+                            // loop through each json object
+                            jsonResponse = "yes";
+                            // Toast.makeText(MainActivity.this, "entro", Toast.LENGTH_SHORT).show();
+                            //hidepDialog();
+
+                            //     System.out.println(jsonResponse);
+                            final ArrayList<String> idservidores=new ArrayList<>();
+                            ArrayList<String> catspre=new ArrayList<>();
+                            final ArrayList<String> nombrespre=new ArrayList<>();
+                            final ArrayList<String> ciudadespre=new ArrayList<>();
+                            final ArrayList<String> estados=new ArrayList<>();
+                            final ArrayList<String> descripciones=new ArrayList<>();
+                            final ArrayList<String> precios=new ArrayList<>();
+                            final ArrayList<String> formapagos=new ArrayList<>();
+                            final ArrayList<String> experiencias=new ArrayList<>();
+                            final ArrayList<String> zonass=new ArrayList<>();
+                            final ArrayList<String> telefonos=new ArrayList<>();
+                            final ArrayList<Double> latitudes=new ArrayList<>();
+                            final ArrayList<Double> longitudes=new ArrayList<>();
+                            final ArrayList<Integer> imagespre =new ArrayList<>();
+                            final ArrayList<String> profesionespre=new ArrayList<>();
+                            final ArrayList<String> distancespre=new ArrayList<>();
+
+                            for (int i = 0; i < response.length(); i++) {
+
+                                JSONObject person = (JSONObject) response
+                                        .get(i);
+                                idservidor = person.getString("idservidor");
+                                cat = person.getString("cat");
+                                nombre = person.getString("nombre");
+                                ciudad = person.getString("ciudad");
+                                estado = person.getString("estado");
+                                descripcion = person.getString("descripcion");
+                                precio = person.getString("precio");
+                                formapago = person.getString("formapago");
+                                experiencia = person.getString("experiencia");
+                                zonas = person.getString("zonas");
+                                reputacion = person.getString("reputacion");
+                                telefono = person.getString("telefono");
+                                latitud = person.getDouble("lat");
+                                longitud = person.getDouble("lng");
+                                distance = person.getDouble("distance");
+
+                                String distauxiliar= distance.toString().substring(0,3);
+                                imagespre.add(imagenesdefault[i]);
+
+                                idservidores.add(idservidor);
+                                catspre.add(cat);
+                                nombrespre.add(nombre);
+                                ciudadespre.add(ciudad);
+                                estados.add(estado);
+                                descripciones.add(descripcion);
+                                precios.add(precio);
+                                formapagos.add(formapago);
+                                experiencias.add(experiencia);
+                                zonass.add(zonas);
+                                profesionespre.add(reputacion); //ESTE ES REPUTACIOOOONNN
+                                telefonos.add(telefono);
+                                latitudes.add(latitud);
+                                longitudes.add(longitud);
+                                distancespre.add(distauxiliar);
+                            }
+
+                            //datos para grid
+                            images=imagespre;
+                            nombres=nombrespre;
+                            profesiones=profesionespre;
+                            ciudades=ciudadespre;
+                            distances=distancespre;
+                            cats=catspre;
+
+                            //datos para intent
+                            gridView.setAdapter(new ImageAdapter(getApplicationContext(),
+                                    nombres,images,profesiones,ciudades,distances,cats));
+
+                            gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                                    Intent vete = new Intent(getApplicationContext(),categoria.class);
+                                    vete.putExtra("imagen",imagespre.get(position));
+                                    vete.putExtra("idservidor",idservidores.get(position));
+                                    vete.putExtra("cat",cats.get(position));
+                                    vete.putExtra("nombre",nombrespre.get(position));
+                                    vete.putExtra("ciudad",ciudadespre.get(position));
+                                    vete.putExtra("estado",estados.get(position));
+                                    vete.putExtra("descripcion",descripciones.get(position));
+                                    vete.putExtra("precio",precios.get(position));
+                                    vete.putExtra("formapago",formapagos.get(position));
+                                    vete.putExtra("experiencia",experiencias.get(position));
+                                    vete.putExtra("zonas",zonass.get(position));
+                                    vete.putExtra("reputacion",profesionespre.get(position));
+                                    vete.putExtra("telefono",telefonos.get(position));
+                                    vete.putExtra("latitud",latitudes.get(position));
+                                    vete.putExtra("longitud",longitudes.get(position));
+                                    vete.putExtra("distance",distancespre.get(position));
+
+                                    startActivity(vete);
+
+
+
+                                    // Toast.makeText(cercanos.this, "Tocaste un grid", Toast.LENGTH_SHORT).show();
+
+                                }
+                            });
+
+
+
+
+
+
+
+
+
+
+
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(getApplicationContext(),
+                                    "Error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+
+                        hidepDialog();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+                hidepDialog();
+            }
+        });
+
+        req.setShouldCache(false);
+        Volley.newRequestQueue(getApplicationContext()).add(req);
+    }
+
+
+
+}
--- app/src/main/java/com/example/marci/hf1/categorias.java
+++ app/src/main/java/com/example/marci/hf1/categorias.java
@@ -1,8 +1,13 @@
 package com.example.marci.hf1;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,68 +16,105 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 
 public class categorias extends Fragment {
 
-    LinearLayout belleza;
+    LinearLayout categoriasss,categoria;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
+
         View vista = inflater.inflate(R.layout.nuevocat, container, false);
-        belleza=(LinearLayout)vista.findViewById(R.id.bellezaCat);
-
-        belleza.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent vete = new Intent(getContext(),detalle_categorias.class);
-                vete.putExtra("nombre","Belleza");
-
-                startActivity(vete);
-            }
-        });
-
-        
-
-       /*&& Display display = getActivity().getWindowManager().getDefaultDisplay();
-        int stageWidth = display.getWidth();
-
-        Toast.makeText(getContext(), ""+stageWidth, Toast.LENGTH_SHORT).show();
-        LinearLayout catr = (LinearLayout)vista.findViewById(R.id.categoriarosa);
-        LinearLayout catce = (LinearLayout)vista.findViewById(R.id.categoriaceleste);
-        LinearLayout catmo = (LinearLayout)vista.findViewById(R.id.categoriamorada);
-        LinearLayout catam = (LinearLayout)vista.findViewById(R.id.categoriaamarilla);
-
-        ViewGroup.LayoutParams lp1 = catr.getLayoutParams();
-        lp1.width=stageWidth/3;
-
-        ViewGroup.LayoutParams lp2 = catce.getLayoutParams();
-        lp2.width=stageWidth/3;
-
-        ViewGroup.LayoutParams lp3 = catmo.getLayoutParams();
-        lp3.width=stageWidth/3;
-
-        ViewGroup.LayoutParams lp4 = catam.getLayoutParams();
-        lp4.width=stageWidth/3;
-*/
-
-/*        LinearLayout ll1 = (LinearLayout)vista.findViewById(R.id.ll1);
-        LinearLayout ll2 = (LinearLayout)vista.findViewById(R.id.ll2);
-        LinearLayout ll3 = (LinearLayout)vista.findViewById(R.id.ll3);*/
-        LinearLayout lay = (LinearLayout)vista.findViewById(R.id.result1);
-       // TextView tv = (TextView)vista.findViewById(R.id.categName);
-      //  tv.setText(""+lay.getMeasuredWidth());
 
 
-        return vista;
+        final ArrayList<Integer> imagenes= new ArrayList();
+        imagenes.add(R.drawable.belleza);
+        imagenes.add(R.drawable.cuidador);
+        imagenes.add(R.drawable.deportes);
+        imagenes.add(R.drawable.entertainment);
+        imagenes.add(R.drawable.hogar);
+        imagenes.add(R.drawable.mascotas);
+        imagenes.add(R.drawable.otros);
+        imagenes.add(R.drawable.profesor);
+        imagenes.add(R.drawable.reparacion);
+        imagenes.add(R.drawable.salud);
+        imagenes.add(R.drawable.transporte);
+
+        final ArrayList<String> nombres = new ArrayList();
+        nombres.add(getString(R.string.Belleza));
+        nombres.add(getString(R.string.Cuiddor));
+        nombres.add(getString(R.string.Deporte));
+        nombres.add(getString(R.string.Entretenimiento));
+        nombres.add(getString(R.string.Hogar));
+        nombres.add(getString(R.string.Mascotas));
+        nombres.add(getString(R.string.Otros));
+        nombres.add(getString(R.string.Profesores));
+        nombres.add(getString(R.string.Reparaciones));
+        nombres.add(getString(R.string.Salud));
+        nombres.add(getString(R.string.Transporte));
+
+        final ArrayList<String> descripcion = new ArrayList();
+        descripcion.add(getString(R.string.BellezaDesc));
+        descripcion.add(getString(R.string.CuidadorDesc));
+        descripcion.add(getString(R.string.DeporteDesc));
+        descripcion.add(getString(R.string.EntretenimientoDesc));
+        descripcion.add(getString(R.string.HogarDesc));
+        descripcion.add(getString(R.string.MascotasDesc));
+        descripcion.add(getString(R.string.OtrosDesc));
+        descripcion.add(getString(R.string.ProfesoresDesc));
+        descripcion.add(getString(R.string.ReparacionesDesc));
+        descripcion.add(getString(R.string.SaludDesc));
+        descripcion.add(getString(R.string.TransporteDesc));
+
+        categoriasss =  vista.findViewById(R.id.LLroot);
+
+        for (int i=0; i<nombres.size();i++) {
+            categoria = (LinearLayout) LayoutInflater.from(getContext()).inflate(R.layout.nuevocat_element, null);
+            LinearLayout base =  categoria.findViewById(R.id.nuevoelemento);
+            ViewGroup.LayoutParams params = base.getLayoutParams();
+
+          int dp=  (int) (170 * getContext().getResources().getDisplayMetrics().density + 0.5f);
+
+            LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(
+                    /*width*/ ViewGroup.LayoutParams.MATCH_PARENT,
+                   /*height*/ dp
+            );
+
+            base.setLayoutParams(param);
+
+            final TextView nombre =  categoria.findViewById(R.id.nombreCAT);
+            TextView desc =  categoria.findViewById(R.id.desCAT);
+            base.setBackground(getResources().getDrawable(imagenes.get(i)));
+            nombre.setText(nombres.get(i));
+            desc.setText(descripcion.get(i));
+
+            final int finalI = i;
+            final int finalI1 = i;
+            categoria.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent go = new Intent(getContext(),categoria_result.class);
+                    go.putExtra("categoria",nombres.get(finalI1).toString().toUpperCase());
+                    startActivity(go);
+                }
+            });
+
+            categoriasss.addView(categoria);
+        }
+
+            return vista;
 
     }
 
