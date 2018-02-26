827bdce777bfea17f5f13ba4e8851d270e1c9419
Miguel Sanchez <sanchezmj228@gmail.com>
2018-02-25 21:23:19 -0400

    #actionbar-navdrawer/categorias-result

--- app/src/main/java/com/example/marci/hf1/categoria_result.java
+++ app/src/main/java/com/example/marci/hf1/categoria_result.java
@@ -5,13 +5,21 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -21,6 +29,7 @@ import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -28,7 +37,9 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-public class categoria_result extends AppCompatActivity {
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class categoria_result extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     // json object response url
     private String urlJsonObj = "https://api.androidhive.info/volley/person_object.json";
@@ -56,21 +67,107 @@ public class categoria_result extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.nuevocer);
+        setContentView(R.layout.activity_nuevocer);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         gridView = findViewById(R.id.grid);
         cate = getIntent().getExtras().getString("categoria");
         lon=MainActivity.lon;
         lat=MainActivity.lat;
-
+        this.setTitle(cate);
         pDialog = new ProgressDialog(categoria_result.this);
         pDialog.setMessage("Filtrando cercanos...");
         pDialog.setCancelable(false);
         makeJsonArrayRequest();
         System.out.println(jsonResponse);
 
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        View header = navigationView.getHeaderView(0);
+
+        TextView navuser= header.findViewById(R.id.profile_name_nav);
+        CircleImageView imageView = header.findViewById(R.id.profile_image_nav);
+/*        if (llego){
+            navuser.setText(nombreuserfinal);
+            Glide.with(getApplicationContext()).load(imagenfinal).centerCrop().into(imageView);
+
+        }*/
+
         }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        FragmentManager fragmentManager= getSupportFragmentManager();
+
+        if (id == R.id.menu_home) {
+            fragmentManager.beginTransaction().replace(R.id.contenedor,new fragmentMain()).commit();
+        } else if (id == R.id.menu_notif) {
+            fragmentManager.beginTransaction().replace(R.id.contenedor,new categorias()).commit();
+        } else if (id == R.id.menu_fav) {
+            fragmentManager.beginTransaction().replace(R.id.contenedor,new destacados()).commit();
+        } else if (id == R.id.misAnuncios) {
+            fragmentManager.beginTransaction().replace(R.id.contenedor,new cercanos()).commit();
+        } else if (id == R.id.miCuenta) {
+
+        } else if (id == R.id.menureport) {
+
+        } else if (id == R.id.menuacercade) {
+
+        }
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
 
 
 
